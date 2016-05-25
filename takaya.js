301841bd0b48b2a797643c2031229be7ac46580f
unknown <>
2016-05-25 13:08:39 +0900

    精度向上

--- takaya.js
+++ takaya.js
@@ -1,20 +1,30 @@
 var x;
 var y;
 
+function ca(c) {
+  if (scan(c + 3, 5) < 70) {
+    cannon(c + 6, scan(c + 3, 20));
+  } else if (scan(c - 3, 5) < 70) {
+   cannon(c - 6, scan(c - 3, 20)); 
+  } else if (scan(c, 5) < 70) {
+    cannon(c, scan(c, 20));
+  }
+  }
+
 x = 0;
 y = 0;
 while (true) {
   x = (typeof x == 'number' ? x : 0) + 17;
   y = (typeof y == 'number' ? y : 0) + 9;
-  if (scan(x) < 30) {
-    swim(x - 180);
-    cannon(x, scan(x));
-  } else if (scan(x + 123) < 20) {
-    swim(x - 33);
-    cannon(x + 123, scan(x + 123));
-  } else if (scan(x - 117 ) < 20) {
-    swim(x - 207);
-    cannon(x - 117, scan(x - 117));
+  if (scan(x, 12) < 30) {
+    swim(x - 150);
+    ca(x);
+  } else if (scan(x + 123, 12) < 30) {
+    swim(x - 3);
+    ca(x + 123);
+  } else if (scan(x - 117, 12) < 30) {
+    swim(x - 177);
+    ca(x - 117);
   }  else if (loc_y() > 95) {
     swim(200);
   } else if (loc_x() > 95) {
@@ -23,14 +33,14 @@ while (true) {
     swim(20);
   } else if (loc_x() < 5) {
     swim(290);
-  } else if (scan(y) < 70) {
-    swim(y - 30);
-    cannon(y, scan(y));
-  } else if (scan(y + 120) < 70) {
-    swim(y + 90);
-    cannon(y + 120, scan(y + 120));
-  } else if (scan(y - 120) < 70) {
-    swim(y - 150);
-    cannon(y - 120, scan(y + 120));
+  } else if (scan(y, 12) < 70) {
+    swim(y - 40);
+    ca(y);
+  } else if (scan(y + 123, 12) < 70) {
+    swim(y + 80);
+    ca(y + 123);
+  } else if (scan(y - 117, 12) < 70) {
+    swim(y - 160);
+    ca(y - 117);
   }
 }
